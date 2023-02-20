6eda33d08a4ef2e814893686edebd098f6db0f21
Andrey Vasnetsov <andrey@vasnetsov.com>
2023-02-20 12:15:48 +0100

    fix CI

--- tools/download_and_index.sh
+++ tools/download_and_index.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/../"
 
 git clone https://github.com/qdrant/qdrant.git /tmp/qdrant
 
-bash -x tools/index_qdrant.sh /tmp/qdrant
+QDRANT_PATH=/tmp/qdrant bash -x tools/index_qdrant.sh
 
 rm -rf /tmp/qdrant
 
