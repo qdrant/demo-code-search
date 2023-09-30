134c0dda281658e4f1f0f60bcb2835b759486e2f
Kartik Gupta <kartikgupta7267@gmail.com>
2023-10-01 05:07:48 +0530

    secure proxy rmoved

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -15,7 +15,6 @@ export default defineConfig({
       "/api": {
         target: "https://code-search.qdrant.tech/",
         changeOrigin: true,
-        secure: false, 
       },
     },
   },
