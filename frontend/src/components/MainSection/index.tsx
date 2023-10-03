d5cc1d21c5e50198f8dfa8a8084c53bd2d73da3a
Kartik Gupta <kartikgupta7267@gmail.com>
2023-10-03 10:43:09 +0530

    landing and loader added

--- frontend/src/components/MainSection/index.tsx
+++ frontend/src/components/MainSection/index.tsx
@@ -1,4 +1,15 @@
-import { Button, Container, TextInput, Grid, ScrollArea } from "@mantine/core";
+import {
+  Button,
+  Container,
+  TextInput,
+  Grid,
+  ScrollArea,
+  Box,
+  Image,
+  Title,
+  Text,
+  Loader,
+} from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
 import useMountedState from "@/hooks/useMountedState";
 import { useGetSearchResult } from "@/hooks/useGetSearchResult";
@@ -9,9 +20,10 @@ import classes from "./Main.module.css";
 
 export function Main() {
   const [query, setQuery] = useMountedState("");
-  const { data, getSearch } = useGetSearchResult();
+  const { data, getSearch, loading, error, resetData } = useGetSearchResult();
 
   const handleSubmit = () => {
+    resetData();
     if (query) {
       getSearch(query);
     }
@@ -22,7 +34,6 @@ export function Main() {
       <TextInput
         radius={10}
         size="md"
-        
         leftSection={<IconSearch color="#102252" />}
         placeholder="Enter a query"
         rightSection={
@@ -43,24 +54,56 @@ export function Main() {
         required
         onChange={(event) => setQuery(event.currentTarget.value)}
         onKeyDown={getHotkeyHandler([["Enter", handleSubmit]])}
-        classNames={{input: classes.input}}
+        classNames={{ input: classes.input }}
       />
+      {data && (
+        <Grid>
+          <Grid.Col span={3}>
+            <FileTree data={data} />
+          </Grid.Col>
+          <Grid.Col span={9}>
+            <ScrollArea className={classes.codeDisplayArea}>
+              {data?.result.map((item) => (
+                <CodeContainer
+                  {...item}
+                  key={`${item.context.snippet} ${item.line_from} ${item.line_to}`}
+                />
+              ))}
+            </ScrollArea>
+          </Grid.Col>
+        </Grid>
+      )}
+      {!data && !loading && (
+        <Box>
+          <Image
+            src="/landing.gif"
+            alt="Qdrant Landing"
+            h={400}
+            fit="contain"
+          />
+          <Title order={3} className={classes.heading}>
+            Qdrant <span className={classes.headingHighlight}>Code Search</span>{" "}
+            Unleashing Semantic Power
+          </Title>
+          <Text className={classes.subHeading}>
+            Qdrant Code Explorer: Empowering Semantic Searching in Qdrant
+            Repository with Advanced Code Analysis
+          </Text>
+        </Box>
+      )}
+      {error && <Text>Something went wrong</Text>}
+      {loading && (
+        <Box className={classes.loader}>
+          <Loader type="bars" />
+        </Box>
+      )}
+      {error && (
+        <Box>
+          <Image src="/error.gif" alt="Error" h={400} fit="contain" />
 
-      <Grid>
-        <Grid.Col span={3}>
-          <FileTree data={data} />
-        </Grid.Col>
-        <Grid.Col span={9}>
-          <ScrollArea className={classes.codeDisplayArea}>
-            {data?.result.map((item) => (
-              <CodeContainer
-                {...item}
-                key={`${item.context.snippet} ${item.line_from} ${item.line_to}`}
-              />
-            ))}
-          </ScrollArea>
-        </Grid.Col>
-      </Grid>
+          <Text className={classes.subHeading}>{error}</Text>
+        </Box>
+      )}
     </Container>
   );
 }
