f93eab28ecdff42f63c8e7b0dde616cc8fc5aeaf
kartik-gupta-ij <kartikgupta7267@gmail.com>
2023-11-11 14:17:14 +0530

    Add file search functionality by adding a separate collection for files

--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -1,3 +1,5 @@
 const API_V1 = "api/";
 
 export const SEARCH_URL = `${API_V1}search`;
+
+export const FILE_URL = `${API_V1}file`;
--- tools/index_qdrant.sh
+++ tools/index_qdrant.sh
@@ -11,6 +11,10 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 ROOT_PATH=$SCRIPT_PATH/..
 
 
+python -m code_search.index.files_to_json
+
+python -m code_search.index.file_uploader
+
 rustup run stable rust-analyzer -v lsif $QDRANT_PATH > $ROOT_PATH/data/index.lsif
 
 python -m code_search.index.convert_lsif_index
